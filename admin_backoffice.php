12e433a57e7a12faf2453097638db35ffddce80c
Alionur93 <alionur_93@hotmail.com>
2017-05-22 10:59:47 +0200

    Add files via upload

---
+++ admin_backoffice.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="../css/profil.css"/>
+    <title> Mon profil </title>
+</head>
+<?php include("haut_de_page_backoffice.php"); ?>
+
+<body>
+
+
+
+<body>
+
+
+</body>
+<?php include("bas_de_page.php"); ?>
+
+</html>

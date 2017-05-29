cb157379a8596038dadbb8781a72e22953f1227b
clementmosser <clement-mosser@live.fr>
2017-05-29 15:10:02 +0200

    Modification page d'acceuil et haut de page

--- vue/boutique.php
+++ vue/boutique.php
@@ -1,30 +1,76 @@
 <!DOCTYPE html>
-<html xmlns:justify xmlns:text-justify="http://www.w3.org/1999/xhtml">
+<html lang="en">
+<header>
+    <meta charset="UTF-8">
+    <title>Ma Boutique</title>
+    <link rel="stylesheet" href="../css/Boutique.css"/>
 
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/BoutiqueCSS.css"/>
-    <title>DoHome|Boutique</title>
-    <?php include("haut_de_page.php"); ?>
+    <D>
+        <section>
+            <div class="container">
+                <img class="logo" src="../img/fond_transparent3.png" alt="logo de l'entreprise">
+                <nav>
+                    <ul class="menu">
+                        <li>Dashboard</li>
+                        <li>Mon profil</li>
+                        <li>Ma maison </li>
+                        <li><span id="boutique">Boutique</span> </li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
 
-</head>
+        <section>
+            <div class="recherche">
+                <form action="/search" id="searchthis" method="get">
+                    <input id="search" name="q" type="text" placeholder="Rechercher" />
+                    <input id="search-btn" type="submit" value="Rechercher" />
+                </form>
+            </div>
+        </section>
+
+    </D>
+</header>
 
 
 <body>
-<a>
-    DoHome
-    Entreprise™
-</a>
-<article>
-    <aside>
-        Boutique
-    </aside>
-    Nos Offres
-    Les produits conseillés
-</article>
+<x>
+    <section1>
+        <a href="Maison.php" >
+            <div class="Offres">
+                <p>Nos Offrres</p>
+            </div>
+        </a>
+
+
+    </section1>
+
+    <section2>
+        <a href="Maison.php" >
+            <div class="Promotions">
+                 <p>Nos Promotions </p>
+
+
+             </div>
+        </a>
+
+    </section2>
+
+    <section3>
+        <a href="Maison.php" >
+            <div class="Produits">
+                <p>
+            Tous nos produits
+                </p>
+        </div>
+        </a>
+
+    </section3>
+
+</x>
 </body>
-
-
-<?php include("bas_de_page.php"); ?>
-
+<footer>
+    <div class="Powered">Powered By DoHome Enterprise™</div>
+    <div class="Mentions">Mentions Légales</div>
+</footer>
 </html>

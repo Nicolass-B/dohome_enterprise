ea802ef0b57c3e7246a66391754c3cc3b8c26026
clementmosser <clement-mosser@live.fr>
2017-05-29 15:05:33 +0200

    Modification page d'acceuil et haut de page

--- vue/boutique.php
+++ vue/boutique.php
@@ -1,53 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
-<header>
-    <meta charset="UTF-8">
-    <title>Ma Boutique</title>
-    <link rel="stylesheet" href="../css/Boutique.css"/>
+<html xmlns:justify xmlns:text-justify="http://www.w3.org/1999/xhtml">
 
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="../css/BoutiqueCSS.css"/>
     <title>DoHome|Boutique</title>
     <?php include("haut_de_page.php"); ?>
-</header>
+
+</head>
 
 
 <body>
-<x>
-    <section1>
-        <a href="Maison.php" >
-            <div class="Offres">
-                <p>Nos Offrres</p>
-            </div>
-        </a>
-
-
-    </section1>
-
-    <section2>
-        <a href="Maison.php" >
-            <div class="Promotions">
-                 <p>Nos Promotions </p>
-
-
-             </div>
-        </a>
-
-    </section2>
-
-    <section3>
-        <a href="Maison.php" >
-            <div class="Produits">
-                <p>
-            Tous nos produits
-                </p>
-        </div>
-        </a>
-
-    </section3>
-
-</x>
+<a>
+    DoHome
+    Entreprise™
+</a>
+<article>
+    <aside>
+        Boutique
+    </aside>
+    Nos Offres
+    Les produits conseillés
+</article>
 </body>
 
 

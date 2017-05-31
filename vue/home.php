03561c0e70fd123f6d80acf98dde89bf214c951c
clementmosser <clement-mosser@live.fr>
2017-05-31 12:01:09 +0200

    blabla

--- vue/home.php
+++ vue/home.php
@@ -29,23 +29,7 @@
 
 <div class="corpPage">
     <div class="textIntro">
-
-        L'approche sociale dans notre pays encourage la diversité
-        et la sensibilité d'un mouvement cheminant vers plus d'unité.
-        En effet, c'est en toute prescience que je peux garantir
-        présentement que la sécurité des personnes confirme
-        l'inaliénable volonté d'un temps et d'une époque en phase
-        avec les innovations afin de ne pas être dépassé.
-        Beaucoup de nos concitoyens n’ont pas encore fait leur
-        choix et pensent que la tradition attachée à notre mémoire
-        consubstantielle justifie les subventions publiques d'un
-        déploiement visionnaire revivifiant. De plus, vous
-        connaissez certainement la situation qui mène à dire que
-        la dynamique vertueuse doit correspondre aux directives
-        indispensables préalablement établies afin de poursuivre
-        l'effort plus que légitime d'une majorité parlementaire
-        qui responsabiliserait sur l'urgence écologique.
-
+        <img class="presentation" src="../Vue/img/presentation.png" alt="presentation"/>
     </div>
 
     <div class="connexion-inscription">

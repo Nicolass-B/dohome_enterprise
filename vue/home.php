f00bd96572b7bea80497732195362363c51d77cb
clementmosser <clement-mosser@live.fr>
2017-06-01 02:00:47 +0200

    blabla

--- vue/home.php
+++ vue/home.php
@@ -28,7 +28,7 @@
 
 
 <div class="corpPage">
-    </br><a id="x">Bienvenue sur DoHome Enterprise™</a></br>
+    </br><div class="centre2">Bienvenue sur DoHome Enterprise™</div></br>
     <div class="connexion-inscription">
         <form method="POST" action="../controller/traitementConnexion.php">
 
@@ -52,30 +52,53 @@
         </div>
     </div>
 
+    <aside class="defilement" style="background-image: url('../vue/img/fond.jpg');">
+        <section>
+            <div class="element">
+                <img src="../vue/img/one.png" class="photo">
+                <div class="texte">DoHome Enterprise ™ vous propose </br> une solution domotique innovante </br>et intelligente</div>
+            </div>
+            <div class="element">
+                <img src="../vue/img/two.png" class="photo">
+                <div class="texte">Gérez votre consommation </br> énergétique et faites </br>des bénéfices !</div>
+            </div>
+        </section>
+        <section>
+            <div class="element">
+                <img src="../vue/img/three.png" class="photo">
+                <div class="texte">Contrôlez votre maison </br>où que vous soyez sur votre smartphone </br>ou votre ordinateur</div>
+            </div>
+            <div class="element">
+                <img src="../vue/img/four.png" class="photo">
+                <div class="texte">Toutes vos données sont </br>sécurisées et sont anonymes</div>
+            </div>
+        </section>
+    </aside>
+
+
+
+
+
     <div class="contact" >
         <div class="contact_bis">
-            <div class="centre">
+            <div class="centre2">
                 Contact
             </div>
             <div class="conteneur">
                 <div class="element">
-                    <img src="Vue/Images/map.png" class="contact_icon">
-                    <p class="contact_txt">28 rue notre dame des champs</p>
+                    <img src="../vue/img/localisation.png" class="icone">
+                    <div class="contact_txt">28 Rue Notre Dame des Champs</br>Paris 75006</div>
                 </div>
                 <div class="element">
-                    <img src="Vue/Images/mail.png" class="contact_icon">
-                    <p class="contact_txt">contact@domisep.com</p>
+                    <img src="../vue/img/mail.png" class="icone">
+                    <div class="contact_txt">admin@dohome.com</div>
                 </div>
                 <div class="element">
-                    <img src="Vue/Images/phone.png" class="contact_icon">
-                    <p class="contact_txt">06 00 00 00 00</p>
+                    <img src="../vue/img/telephone.png" class="icone">
+                    <div class="contact_txt">06 XX XX XX XX</div>
                 </div>
             </div>
         </div>
-    </div>
-
-    <div class="textIntro">
-        <img class="presentation" src="../Vue/img/presentation.png" alt="presentation"/>
     </div>
 </div>
 

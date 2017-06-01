bcb46ffe3b277bd8792b4bb3ebc38e26e757367a
clementmosser <clement-mosser@live.fr>
2017-06-01 02:09:22 +0200

    blabla

--- vue/dashboard.php
+++ vue/dashboard.php
@@ -112,10 +112,10 @@
             <div class="information">
                 <div class="video">
                     <p>Vidéosurveillance</p>
-                    <video src="../img/Zombieland copie.mp4" controls poster="../img/fond-decran-page2.jpg" width="200">
+                    <video src="../vue/video/doubleration.mp4" controls poster="../img/fond-decran-page2.jpg" width="200">
                     </video>
                     <p>Salon</p>
-                    <video src="../img/Zombieland copie.mp4" controls poster="../img/fond-decran-page2.jpg" width="200">
+                    <video src="../vue/video/doubleration.mp4" controls poster="../img/fond-decran-page2.jpg" width="200">
                     </video>
                     <p>Chambre parents</p>
                 </div>

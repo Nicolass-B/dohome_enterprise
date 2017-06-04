79ac9df29746ab400aa578c0be737299e801a420
Antoine Poussot <fallenhaswon@gmail.com>
2017-06-04 12:40:32 +0200

    Ajout du recaptcha sur la rédaction de messsage //TODO : le faire également à l'incscription

--- controller/redigermessage.php
+++ controller/redigermessage.php
@@ -11,17 +11,44 @@ include('../vue/redigermessage.php');
 include_once '../modele/messagerie.php';
 
 
-
-if(isset($_POST["Titre"])&& !empty($_POST["Titre"])){
-    if(isset($_POST["contenu"])&& !empty($_POST["contenu"])){
-        if(isset($_POST["destinataire"])&& !empty($_POST["destinataire"])){
+if (isset($_POST["Titre"]) && !empty($_POST["Titre"])) {
+    if (isset($_POST["contenu"]) && !empty($_POST["contenu"])) {
+        if (isset($_POST["destinataire"]) && !empty($_POST["destinataire"])) {
             echo 'POST PASS !';
-            $idDestinataire = getIdFromName($dbh, $_POST['destinataire']);
-            sendMessageToUser($dbh, $_SESSION['idUser'], $idDestinataire, $_POST['Titre'], $_POST['contenu']);
 
-            ?>
-            <script>alert("<?php echo htmlspecialchars('le message est envoyé !', ENT_QUOTES); ?>")</script>
-            <?php
+            if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
+                $secret = '6Ldd3iMUAAAAAHivcpzKVjz-HRtZcdmWwv8iFCIF';
+                $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $_POST['g-recaptcha-response']);
+                $responseData = json_decode($verifyResponse);
+
+                if ($responseData->success) {
+                    // if recaptcha check was success
+
+                    $idDestinataire = getIdFromName($dbh, $_POST['destinataire']);
+                    sendMessageToUser($dbh, $_SESSION['idUser'], $idDestinataire, $_POST['Titre'], $_POST['contenu']);
+
+                    ?>
+                    <script>alert("<?php echo htmlspecialchars('le message est envoyé !', ENT_QUOTES); ?>")</script>
+                    <?php
+                    //$succMsg = 'Your contact request have submitted successfully.';
+                    //exit($succMsg);
+                } else {
+                    // if not show the error
+                    $errMsg = 'Robot verification failed, please try again.';
+                    echo $errMsg;
+
+                }
+
+            } else {
+                // if recaptcha is not checked
+                ?>
+                <script>alert("<?php echo htmlspecialchars('cochez le je ne suis pas un robot', ENT_QUOTES); ?>")</script>
+                <?php
+                $errMsg = 'Please click on the reCAPTCHA box.';
+
+            }
         }
+
     }
 }
+
--- vue/redigermessage.php
+++ vue/redigermessage.php
@@ -10,26 +10,33 @@ include 'haut_de_page.php';
 ?>
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="../css/style_sign_up.css">
+        <link rel="stylesheet" HREF="../css/tableau.css">
+        <link rel="stylesheet" href="../css/messagerie.css">
         <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
         <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+        <script src='https://www.google.com/recaptcha/api.js'></script>
     </head>
 
     <body>
-    <div class="ui-widget">
-        <form method="POST" action="../controller/redigermessage.php" id="message">
-            <label>Objet : </label>
-            <input type="text" name="Titre" placeholder="Sujet du message" required/>
-            </br>
-            <label for="destinataire">Pour : </label>
-            <input type="text" name="destinaire" id="destinataire" placeholder="Destinataire" ">
-            </br>
-            <textarea form="message" name="contenu" id="taid" cols="35" rows="10" wrap="soft"></textarea>
-            <br>
-            <input type="submit" name="envoi" value="Envoyer Le Message"/>
-        </form>
-    </div>
+    <div class="joli-form">
+        <div class="ui-widget">
+            <form method="POST" action="../controller/redigermessage.php" id="message">
+                <label>Objet : </label>
+                <input type="text" name="Titre" placeholder="Sujet du message" required/>
+                </br>
+                <label for="destinataire">Pour : </label>
+                <input type="text" name="destinaire" id="destinataire" placeholder="Destinataire" ">
+                </br>
+                <textarea form="message" name="contenu" id="taid" cols="35" rows="10" wrap="soft"></textarea>
+                <br>
+                <div id="wrapper">
+                    <div class="g-recaptcha" data-sitekey="6Ldd3iMUAAAAABCNW9rU3_6GWdrqLpKllXCOEDnK"></div>
+                    <input type="submit" name="envoi" value="Envoyer Le Message"/>
+                </div>
 
+            </form>
+        </div>
+    </div>
     </body>
 
     <script>

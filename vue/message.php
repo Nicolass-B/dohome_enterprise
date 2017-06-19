36410aa21e3b97ec8ae5bb38ae927cebcaa0d42e
clementmosser <clement-mosser@live.fr>
2017-06-19 11:50:15 +0200

    Modification css message

--- vue/message.php
+++ vue/message.php
@@ -11,23 +11,31 @@ include_once('haut_de_page.php');
 
 ?>
 
+    <head>
+        <link rel="stylesheet" href="../css/message_css.css"/>
+        <meta charset="UTF-8">
+        <title>Message</title>
+    </head>
+
     <body>
+    <div class="section">
+        <div class="message">
+            <p> Objet: <?php echo $message['Titre'] ?> </p>
+            <p> Date d'expédition:  <?php echo $message['Date'] ?></p>
+            <p> Expéditeur: <?php echo $message['Nom'] ?>  </p>
+        </div>
+        <div class="milieu"> Message</div>
+        </br>
+        <div class="message2"
+            <p> <?php echo $message['contenu'] ?> </p>
+        </div>
+        <div class="retour">
+            <h2>
+                <a href="../controller/messagerie.php" style="color: white"> Retour </a>
+            </h2>
 
-    <div>
-        <h1> <?php echo $message['Titre'] ?> </h1>
-        <h2> <?php echo $message['Date'] ?></h2>
-        <h2> <?php echo $message['Nom'] ?>   </h2>
+        </div>
     </div>
-    <div>
-        <p> <?php echo $message['contenu'] ?> </p>
-    </div>
-    <div>
-        <h2>
-            <a href="../controller/messagerie.php" style="color: white"> Retour </a>
-        </h2>
-
-    </div>
-
     </body>
 
 <?php

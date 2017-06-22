5856753422089232307c8accf7da5e131ff6aa29
ali <alionur_93@hotmail.com>
2017-06-22 10:32:41 +0200

    amélioration vue profil sec Amélioration vue mdp oublié Amélioration vue administrateur

---
+++ controller/faq_backoffice.php
@@ -0,0 +1,5 @@
+<?php
+
+if (!isset($_SESSION)) {session_start();}
+require_once '../vue/faq_backoffice.php';
+
---
+++ vue/faq_backoffice.php
@@ -0,0 +1,34 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="../css/profil.css"/>
+    <link rel="stylesheet" href="../css/tableau.css"/>
+    <title> Analyse des comptes </title>
+</head>
+<?php include("haut_de_page_backoffice.php"); ?>
+
+
+<body>
+<div class="faq_backoffice">
+    <div class="information">
+        <ul>
+            <div class="menusec">
+                <li><a href="../controller/analyse_backoffice.php">Analyse des comptes</a></li>
+                <li class="enCours"><a href="../controller/faq_backoffice.php">FAQ</a></li>
+            </div>
+        </ul>
+    </div>
+</div>
+
+</body>
+
+<?php include("bas_de_page.php"); ?>
+
+</html>

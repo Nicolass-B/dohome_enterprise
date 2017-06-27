d1df2e191c17e663d8560311537bcc5c2ead6ca8
Antoine <fallenhaswon@gmail.com>
2017-06-27 23:16:02 +0200

    changes

--- modele/piece.php
+++ modele/piece.php
@@ -16,6 +16,25 @@ function getPiecesfromMaison(PDO $dbh, $idmaison)
     return $data;
 }
 
+function updateCapteurPiece(PDO $dbh){
+    $query = "SELECT t.Valeur, t.ID_capteur, datemesure FROM(
+                                                      SELECT ID_capteur,MAX(Date_Mesure) AS datemesure
+                                                      FROM historique_capteurs
+                                                      GROUP BY ID_capteur) x
+              JOIN historique_capteurs t ON x.ID_capteur = t.ID_capteur
+                    AND x.datemesure = t.Date_Mesure";
+    $sql = $dbh->prepare($query);
+    $sql->execute();
+    $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+    $req = "UPDATE capteurs SET capteurs.Valeur=:valeur WHERE dohome.capteurs.ID_Capteurs=:id";
+    $sql2 = $dbh->prepare($req);
+    foreach ($data as $datum) {
+        $sql2->execute(array(
+            "valeur" => $datum["Valeur"],
+            "id" => $datum["ID_capteur"]
+        ));
+    }
+}
 function getCapteursfromPiece(PDO $dbh, $idpiece)
 {
     // permet ded retourner tous les capteurs d'une pièce avec des trucs fu
